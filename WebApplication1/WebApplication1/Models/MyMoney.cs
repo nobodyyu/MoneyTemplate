6af60319d6d42aca811cfa488e66ec2072f0b042
AKI YU <nobodyyu@gmail.com>
2018-07-13 23:32:02 +0800

    money list view , model init

---
+++ WebApplication1/WebApplication1/Models/MyMoney.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    
+    public class MyMoney
+    {
+        private String _Cata;
+        private String _CreateDate;
+        private String _Amount;
+
+        public MyMoney(String Cata,String CreateDate,String Amount)
+        {
+            _Cata = Cata;
+            _CreateDate = CreateDate;
+            _Amount = Amount;
+        }
+
+
+
+    }
+}
