a23bfd0c32ecf402d0244c3659f1c96e211e67f3
AKI YU <nobodyyu@gmail.com>
2018-07-20 23:31:22 +0800

    CategoryHelper

---
+++ WebApplication1/WebApplication1/Helper/CategoryHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Helper
+{
+    public static class CategoryHelper
+    {
+        public static HtmlString displayCategory(this HtmlHelper htmlHelper, int categoryyy)
+        {
+            var className = "";
+            var categoryyyName = "";
+
+            /*
+             * 類型的「支出」字樣顯現為紅色
+             * 類型的「收入」字樣顯現為藍色
+            */
+            switch (categoryyy)
+            {
+                case 1:
+                    className = "danger";
+                    categoryyyName = "收入";
+                    break;
+                case 2:
+                    className = "primary";
+                    categoryyyName = "支出";
+                    break;
+                 default:
+                    className = "primary";
+                    categoryyyName = "";
+                    break;
+            }
+
+            return new MvcHtmlString($"<span class='text-{className}'>{categoryyyName}</span>");
+        }
+
+    }
+}
